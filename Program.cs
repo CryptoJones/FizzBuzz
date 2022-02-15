8eaf0cf8082381702b7eb1ec3c19de28a1d44bc7
Crypto Jones <cryptojones@gmail.com>
2022-02-15 06:18:07 -0600

    POST 0.1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i = 1; i < 101; i++)
+{	
+#if DEBUG
+	if (i % 3 == 0) { Console.WriteLine(i + " Fizz"); }
+	if (i % 5 == 0) { Console.WriteLine(i + " Buzz"); }
+#else
+	if (i % 3 == 0) { Console.WriteLine("Fizz"); }
+	if (i % 5 == 0) { Console.WriteLine("Buzz"); }
+#endif
+}
