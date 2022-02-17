1e3bd29797570176419f529477e3834c33a16863
Crypto Jones <cryptojones@gmail.com>
2022-02-16 23:37:58 -0600

    Fixed program logic

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 for (int i = 1; i < 101; i++)
-{	
+{
 #if DEBUG
 	if (i % 3 == 0) { Console.WriteLine(i + " Fizz"); }
-	if (i % 5 == 0) { Console.WriteLine(i + " Buzz"); }
+	else if (i % 5 == 0) { Console.WriteLine(i + " Buzz"); }
+	else { Console.WriteLine(i); }
 #else
 	if (i % 3 == 0) { Console.WriteLine("Fizz"); }
-	if (i % 5 == 0) { Console.WriteLine("Buzz"); }
+	else if (i % 5 == 0) { Console.WriteLine("Buzz"); }
+	else { Console.WriteLine(i); }
 #endif
 }
